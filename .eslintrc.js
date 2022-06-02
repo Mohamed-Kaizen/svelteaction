425e54b69715b12cea325c5478e34d888346b74e
Mohamed-Kaizen <m.n.kaizen@gmail.com>
2022-06-02 12:24:46 +0300

    p

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+	root: true,
+	parser: "@typescript-eslint/parser",
+	extends: [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/recommended",
+		"prettier",
+	],
+	plugins: ["@typescript-eslint"],
+	env: {
+		browser: true,
+		node: true,
+	},
+	rules: {
+		camelcase: "off",
+	},
+}
---
+++ packages/shared/index.ts
@@ -0,0 +1,3 @@
+export function useShare() {
+	return "Hello from share"
+}
