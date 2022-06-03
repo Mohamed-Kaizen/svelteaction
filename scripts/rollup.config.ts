464e48a33240c3f2ccf41b158d7f398e1983e4ed
Mohamed-Kaizen <m.n.kaizen@gmail.com>
2022-06-03 07:24:03 +0300

    commit message

---
+++ scripts/rollup.config.ts
@@ -0,0 +1,64 @@
+import { resolve } from "path"
+import typescript from "rollup-plugin-typescript2"
+import dts from "rollup-plugin-dts"
+import { OutputOptions, RollupOptions } from "rollup"
+import fg from "fast-glob"
+import { packages } from "../meta/packages"
+
+const configs: RollupOptions[] = []
+
+for (const { name, external, submodules } of packages) {
+	const functionNames = ["index"]
+
+	if (submodules)
+		functionNames.push(
+			...fg
+				.sync("*/index.ts", { cwd: resolve(`packages/${name}`) })
+				.map((i) => i.split("/")[0])
+		)
+
+	for (const fn of functionNames) {
+		const input =
+			fn === "index"
+				? `packages/${name}/index.ts`
+				: `packages/${name}/${fn}/index.ts`
+
+		const output: OutputOptions[] = [
+			{
+				file: `packages/${name}/dist/${fn}.cjs`,
+				format: "cjs",
+			},
+			{
+				file: `packages/${name}/dist/${fn}.mjs`,
+				format: "es",
+			},
+		]
+
+		configs.push({
+			input,
+			output,
+			plugins: [
+				typescript({
+					tsconfigOverride: {
+						compilerOptions: {
+							declaration: false,
+						},
+					},
+				}),
+			],
+			external: ["@svelte-use/shared", ...(external || [])],
+		})
+
+		configs.push({
+			input,
+			output: {
+				file: `packages/${name}/dist/${fn}.d.ts`,
+				format: "es",
+			},
+			plugins: [dts()],
+			external: ["@svelte-use/shared", ...(external || [])],
+		})
+	}
+}
+
+export default configs
