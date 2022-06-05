6904952c2ab18ed56a0c7733ffbbece05dfb1dbb
Mohamed-Kaizen <m.n.kaizen@gmail.com>
2022-06-05 11:22:42 +0300

    sdadasd

--- packages/integrations/useIntegrations/index.test.ts
+++ packages/integrations/useIntegrations/index.test.ts
@@ -3,6 +3,6 @@ import { useIntegrations } from "."
 describe("useCore", () => {
 	test("work", () => {
 		const header = useIntegrations()
-		expect(header).toBe("Hello from integrations")
+		expect(header).toBe("Hello from integrations?")
 	})
 })
