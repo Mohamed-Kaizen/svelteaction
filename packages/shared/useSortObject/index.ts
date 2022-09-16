07ff7a2859a4f16ca9c134b8f34ebf5d8650d290
Mohamed-Kaizen <m.n.kaizen@gmail.com>
2022-09-16 19:37:23 +0300

    chore: release v3.0.11

---
+++ packages/shared/useSortObject/index.ts
@@ -0,0 +1,8 @@
+export function useSortObject(obj: { [key: string]: any }) {
+	return Object.keys(obj)
+		.sort()
+		.reduce(function (result: { [key: string]: any }, key: string) {
+			result[key] = obj[key]
+			return result
+		}, {})
+}
