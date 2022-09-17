078420d43bcd3d8d1652c85f3e3b14df81720795
Mohamed-Kaizen <m.n.kaizen@gmail.com>
2022-09-17 13:21:07 +0300

    updates

--- docs/src/routes/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import { useToggle } from "@svelteaction/core"
-	const [toggle, setToggle] = useToggle(false)
-</script>
-
-<h1>Welcome to SvelteKit {toggle}</h1>
-<p>
-	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
